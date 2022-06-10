31e79be036d9a6da13105410f736fa8a2d8cdd02
Hong yeonggwang <chamsk0808@naver.com>
2022-06-10 09:30:59 +0900

    코드 정리

--- login_sys.py
+++ login_sys.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 class User:
     id = ''
@@ -50,62 +49,3 @@ def make_id(userInputId,userInPutPwd):
     user.append(User(userInputId,userInPutPwd))
 class QMessageBox:
     pass
-
-
-def main():
-    global user
-    # user = []
-    loadData()
-
-    while True:
-        print('-----------------------')
-        print('1. 로그인')
-        print('2. 회원가입')
-        print('3. 비밀번호 변경')
-        print('4. 프로그램 종료')
-        print('-----------------------')
-
-        select_no = int(input('옵션을 선택하세요. : '))
-
-        if select_no == 1:
-            cnt = 0
-
-            while True:
-                userInputId = str(input('아이디를 입력하세요 : '))
-                userInputPwd = str(input('비밀번호를 입력하세요 : '))
-                for a in user:
-                    if a.id == userInputId and a.pwd == userInputPwd:
-                        print('로그인 성공, 환영합니다!')
-                        break
-                    else:
-                        cnt = cnt + 1
-                        print('로그인 {}회 실패'.format(cnt))
-
-                if cnt >= 3:
-                    print('로그인 3회 실패로 인해 프로그램을 종료합니다!')
-                    sys.exit()
-
-                print()
-
-        if select_no == 2:
-            make_id()
-            print('회원가입을 성공하였습니다. 환영합니다.\n')
-
-
-        # if select_no == 3:
-        #     uid = str(input('회원 아이디 : '))
-        #     if uid in user:
-        #         print(f'{uid} / {user[uid]}')
-        #         n_pwd = edit_password(uid, user[uid])
-        #         user[uid] = n_pwd
-        #         print('비밀번호가 변경되었습니다.\n')
-        #     else:
-        #         print('등록된 아이디가 아닙니다.\n')
-        #         continue
-
-        if select_no == 4:
-            saveData()
-            print('프로그램을 종료합니다.')
-            break
-
-# main()
